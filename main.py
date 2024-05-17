ad04445a1de7b44bde9b4f1f31c64ece10249c20
Alexey Karcev <116674575+krawwwwy@users.noreply.github.com>
2024-05-17 08:47:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def generate_random_tree_with_nodes(num_nodes):
         return None
 
     values = [(i, random.randint(1, 10) if random.random() > 0.1 else None) for i in range(num_nodes)]
-    values[0] = (0, random.randint(1, 10))  
+    values[0] = (0, random.randint(1, 10))  # Root node must be non-null
 
     def build_tree(index):
         if index >= num_nodes or values[index][1] is None:
@@ -96,7 +96,7 @@ def find_subtree_structure(root, subtree):
 def build_anytree(root, parent=None):
     if not root:
         return None
-    node = Node(str(root.val), parent=parent)  
+    node = Node(str(root.val), parent=parent)  # Используем значения узлов для вывода
     if root.left:
         build_anytree(root.left, parent=node)
     if root.right:
@@ -107,15 +107,16 @@ def build_anytree(root, parent=None):
 def print_tree(root):
     anytree_root = build_anytree(root)
     if anytree_root:
-        for pre, fill, node in RenderTree(anytree_root, style=AsciiStyle()):
+        for pre, fill, node in RenderTree(anytree_root):
             print("%s%s" % (pre, node.name))
     else:
         print("Empty tree")
 
 
+# Основная часть программы
 if __name__ == "__main__":
-    num_main_nodes = 10000000  
-    num_sub_nodes =  15 
+    num_main_nodes = int(input("Введите количество узлов в основном дереве: "))  # Количество узлов в основном дереве
+    num_sub_nodes = int(input("Введите количество узлов в поддереве: ")) # Количество узлов в поддереве
 
 
 
@@ -124,11 +125,11 @@ if __name__ == "__main__":
     main_tree = read_tree_from_file('main_tree.json')
     subtree = read_tree_from_file('subtree.json')
 
-    #print("Main Tree:")
-    #print_tree(main_tree)
+    print("Main Tree:")
+    print_tree(main_tree)
 
-    #print("\nSubtree:")
-    ##print_tree(subtree)
+    print("\nSubtree:")
+    print_tree(subtree)
 
     matches = find_subtree_structure(main_tree, subtree)
     print("\nFound Subtrees:")
